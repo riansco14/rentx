dc0856f9701f320139d1869816c0ce131d007617
riansco14 <rian.rabelo@ufrpe.br>
2021-08-12 23:19:07 -0300

    Instalação Fontes e Tema Global

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo';
 import { Home } from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/global/styles/theme';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts([Inter_400Regular, Inter_500Medium, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold])
+
+  if (!fontsLoaded) {
+    <AppLoading />
+  }
+
   return (
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
