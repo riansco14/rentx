86bdb3ba85b9800bc6c2ee448deeca19121da748
riansco14 <rian.rabelo@ufrpe.br>
2021-08-19 00:31:37 -0300

    Detalhes Carro - Adicionando mais elementos visuais

--- src/screens/DetalhesCarro/index.tsx
+++ src/screens/DetalhesCarro/index.tsx
@@ -1,19 +1,53 @@
 import React from 'react'
+import { ScrollView } from 'react-native'
 import { BackButton } from '../../components/BackButton'
 import { ImageSlider } from '../../components/ImageSlider'
 
-import { CarroImages, Container, Header } from './styles'
+import {
+    CarroImages, Container, Header, 
+    Descricao,
+    Detalhes,
+    Marca,
+    Nome,
+    Alugar,
+    AlugarPeriodo,
+    AlugarPreco,
+    Sobre
+} from './styles'
 
 export function DetalhesCarro() {
     return (
         <Container>
             <Header>
-                <BackButton />
+                <BackButton onPress={() => console.log("Clicou")} />
             </Header>
             <CarroImages>
 
-                <ImageSlider imagesUrl={["https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png","https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png","https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png","https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png"]} />
+                <ImageSlider imagesUrl={["https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png", "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png", "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png", "https://carsguide-res.cloudinary.com/image/upload/f_auto,fl_lossy,q_auto,t_default/v1/editorial/vhs/Audi-S5.png"]} />
             </CarroImages>
+
+
+
+            <ScrollView
+                contentContainerStyle={{ padding: 24, alignItems: 'center' }}
+                showsVerticalScrollIndicator={false}
+            >
+                <Detalhes>
+                    <Descricao>
+                        <Marca>Lamborghini</Marca>
+                        <Nome>Huracan</Nome>
+                    </Descricao>
+                    <Alugar>
+                        <AlugarPeriodo>Ao dia</AlugarPeriodo>
+                        <AlugarPreco>R$ 580</AlugarPreco>
+                    </Alugar>
+                </Detalhes>
+
+                <Sobre>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae quidem dolores incidunt unde quas nisi expedita, sed distinctio architecto officiis. Velit vel reiciendis inventore aspernatur dicta deserunt, eaque harum sit.
+
+                </Sobre>
+            </ScrollView>
         </Container>
     )
 }
--- src/screens/DetalhesCarro/styles.ts
+++ src/screens/DetalhesCarro/styles.ts
@@ -1,8 +1,9 @@
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
     flex:1;
-    background-color: ${({theme})=>theme.colors.background_secondary};
+    background-color: ${({ theme }) => theme.colors.background_secondary};
 `
 
 export const Header = styled.View`
@@ -17,4 +18,44 @@ export const Header = styled.View`
 `
 export const CarroImages = styled.View`
     margin-top: 32px;
+`
+
+export const Detalhes = styled.View`
+    
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+`
+export const Descricao = styled.View``
+export const Marca = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.secondary_500};
+    font-size: ${RFValue(10)}px;
+    color: ${({ theme }) => theme.colors.text_detail};
+    
+    text-transform: uppercase;
+`
+export const Nome = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.secondary_500};
+    font-size: ${RFValue(25)}px;
+    color: ${({ theme }) => theme.colors.title};
+`
+export const Alugar = styled.View``
+export const AlugarPeriodo = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.secondary_500};
+    font-size: ${RFValue(10)}px;
+    color: ${({ theme }) => theme.colors.text};
+    text-transform: uppercase;
+`
+export const AlugarPreco = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.secondary_500};
+    font-size: ${RFValue(25)}px;
+    color: ${({ theme }) => theme.colors.main};
+`
+export const Sobre = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.primary_400};
+    color: ${({ theme }) => theme.colors.text};
+    font-size: ${RFValue(15)}px;
+    text-align: justify;
+
+    margin-top: 24px;
 `
