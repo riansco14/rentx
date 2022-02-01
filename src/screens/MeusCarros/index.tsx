7f89f6fa0f2ca1c933d20847148913cd37dc02d9
riansco14 <rian.rabelo@ufrpe.br>
2022-02-01 11:55:06 -0300

    Tela MeusCarros

---
+++ src/screens/MeusCarros/index.tsx
@@ -0,0 +1,81 @@
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect, useState } from 'react'
+import { StatusBar } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { useTheme } from 'styled-components'
+
+import { BackButton } from '../../components/BackButton'
+import { Carro } from '../../components/Carro'
+import { CarroDTO } from '../../dtos/CarroDTO'
+import api from '../../services/api'
+
+import { Container, Content, Header, SubTitle, Title, Agendamentos, AgendamentosTitulo, AgendamentosQuantidade } from './styles'
+
+
+interface CarroProps{
+    id: string
+    user_id: string
+    car: CarroDTO
+}
+
+export function MeusCarros() {
+    const navigation = useNavigation()
+    const theme = useTheme()
+    const [carros, setCarros] = useState<CarroProps[]>([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function fetchCarros() {
+            try {
+                const response = await api.get("/schedules_byuser?user_id=1")
+
+                setCarros(response.data)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        fetchCarros()
+    }, [])
+
+    function handleGoBack() {
+        navigation.goBack();
+    }
+
+    return (
+        <Container>
+            <Header>
+                <StatusBar
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton color={theme.colors.shape} onPress={handleGoBack} />
+
+                <Title>
+                    Escolha uma data{'\n'}
+                    data de ínicio e{'\n'}
+                    fim do aluguel
+                </Title>
+
+                <SubTitle>
+                    Conforto, segurança e praticidade
+                </SubTitle>
+            </Header>
+
+            <Content>
+                <Agendamentos>
+                    <AgendamentosTitulo>Agendamentos feitos: </AgendamentosTitulo>
+                    <AgendamentosQuantidade>05</AgendamentosQuantidade>
+                </Agendamentos>
+                    <FlatList
+                        data={carros}
+                        keyExtractor={(item: CarroProps) => item.id}
+                        showsVerticalScrollIndicator={false}
+                        renderItem={({item}:{item: CarroProps})=><Carro data={item.car}></Carro>}
+                    />
+            </Content>
+        </Container>
+    )
+}
---
+++ src/screens/MeusCarros/styles.ts
@@ -0,0 +1,58 @@
+import { RFValue } from 'react-native-responsive-fontsize'
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+    flex:1;
+    align-items: center;
+    background-color: ${({ theme }) => theme.colors.background_primary};
+`
+
+export const Header = styled.View`
+    width: 100%;
+    height: 325px;
+    background-color: ${({ theme }) => theme.colors.header};
+    justify-content: center;
+    padding: 25px;
+`
+
+export const Title = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.secondary_600};
+    font-size: ${RFValue(30)}px;
+    margin-top: 24px;
+`
+
+
+export const SubTitle = styled.Text`
+    color: ${({ theme }) => theme.colors.shape};
+    font-family: ${({ theme }) => theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+    margin-top: 24px;
+`
+
+export const Content = styled.View`
+    flex: 1;
+    width: 100%;
+    padding: 0 16px;
+`
+
+export const Agendamentos = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 24px 0;
+`
+
+export const AgendamentosTitulo = styled.Text`
+     color: ${({ theme }) => theme.colors.text};
+    font-family: ${({ theme }) => theme.fonts.primary_400};
+    font-size: ${RFValue(15)}px;
+`
+
+export const AgendamentosQuantidade = styled.Text`
+     color: ${({ theme }) => theme.colors.title};
+    font-family: ${({ theme }) => theme.fonts.primary_500};
+    font-size: ${RFValue(15)}px;
+`
