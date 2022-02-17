efddb8bceb85b1f304e7b0debca36823bdaa494f
riansco14 <rian.rabelo@ufrpe.br>
2022-02-16 21:30:04 -0300

    Slider de carros

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { ViewToken } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler'
 
 import {
     Container,
@@ -13,19 +15,42 @@ interface Props {
     imagesUrl: string[]
 }
 
+interface ChangeImageProps{
+    viewableItems: ViewToken[]
+    changed: ViewToken[]
+}
+
 export function ImageSlider({ imagesUrl }: Props) {
+    const [imageIndex, setImageIndex] = useState(0)   
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        const value = info.viewableItems.find(item=>item.isViewable===true)
+        setImageIndex(value?.index!);
+        console.log(value?.index);
+    })
+    
     return (
         <Container>
             <ImageIndexes>
-                <ImageIndex active={true} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
+                {imagesUrl.map((item, index) => (
+                     <ImageIndex key={String(index)} active={index===imageIndex} />)
+                )}
             </ImageIndexes>
 
-            <CarroImageContainer>
-                <CarroImage source={{ uri: imagesUrl[0] }} />
-            </CarroImageContainer>
+
+            <FlatList
+                data={imagesUrl}
+                keyExtractor={(item,index)=>String(index)}
+                onViewableItemsChanged={indexChanged.current}
+                renderItem={({ item }) => (
+                    <CarroImageContainer>
+                        <CarroImage source={{ uri: item }} resizeMode='cover' />
+                    </CarroImageContainer>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+            ></FlatList>
+
+
 
         </Container>
     )
