acfc8f861c68d928306d448098af4494c85d997f
riansco14 <rian.rabelo@ufrpe.br>
2022-02-16 21:29:12 -0300

    component load

--- src/components/Load/index.tsx
+++ src/components/Load/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {Container} from './styles'
 import { ActivityIndicator } from 'react-native'
 import { useTheme } from 'styled-components'
+
 
 
 export function Load() {
     const theme = useTheme()
 
     return (
-        <ActivityIndicator color={theme.colors.main} size="large" />
+        <Container>
+            <ActivityIndicator color={theme.colors.main} size="large" />
+        </Container>
     )
 }
