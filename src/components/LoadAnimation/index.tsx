abefeeb76ecce2f95e0fec1ad44dba957d692f48
riansco14 <rian.rabelo@ufrpe.br>
2022-03-10 00:10:39 -0300

    LoadAnimation feita em Lottie

---
+++ src/components/LoadAnimation/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Container } from './styles'
+import LottieView from 'lottie-react-native'
+import loadingCar from '../../assets/loadingCar.json'
+
+export function LoadAnimation() {
+    return (
+        <Container>
+            <LottieView
+                source={loadingCar}
+                autoPlay
+                style={{height:200}}
+                resizeMode='contain'
+                loop
+            ></LottieView>
+
+        </Container>
+    )
+}
