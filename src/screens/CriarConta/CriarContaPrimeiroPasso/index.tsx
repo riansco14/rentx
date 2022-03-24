e30b20b1f1fc5257c92ff01f8f4f8b699382c096
riansco14 <rian.rabelo@ufrpe.br>
2022-03-24 00:40:43 -0300

    Primeira Etapa de Criar conta

--- src/screens/CriarConta/CriarContaPrimeiroPasso/index.tsx
+++ src/screens/CriarConta/CriarContaPrimeiroPasso/index.tsx
@@ -1,9 +1,13 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
+import { Keyboard, KeyboardAvoidingView } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 import { BackButton } from '../../../components/BackButton'
 import { Bullet } from '../../../components/Bullet'
+import { Button } from '../../../components/Button'
+import { Input } from '../../../components/Input'
 
-import { Container, Header, Steps, Title,SubTitle } from './styles'
+import { Container, Header, Steps, Title, SubTitle, Form, FormTitle } from './styles'
 
 export function CriarContaPrimeiroPasso() {
     const navigation = useNavigation()
@@ -12,25 +16,54 @@ export function CriarContaPrimeiroPasso() {
         navigation.goBack()
     }
 
-    return (
-        <Container>
-            <Header>
-                <BackButton onPress={handleBack} />
-                <Steps>
-                    <Bullet active />
-                    <Bullet />
-                </Steps>
-            </Header>
+    return (<KeyboardAvoidingView behavior='position' enabled>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} >
+            <Container>
+                <Header>
+                    <BackButton onPress={handleBack} />
+                    <Steps>
+                        <Bullet active />
+                        <Bullet />
+                    </Steps>
+                </Header>
 
-            <Title>
-                Crie sua{'\n'}conta
-            </Title>
-            
-            <SubTitle>
-                Faça seu cadastro de{'\n'}
-                forma rápida e fácil
-            </SubTitle>
+                <Title>
+                    Crie sua{'\n'}conta
+                </Title>
 
-        </Container>
+                <SubTitle>
+                    Faça seu cadastro de{'\n'}
+                    forma rápida e fácil
+                </SubTitle>
+
+                <Form>
+                    <FormTitle>1. Dados</FormTitle>
+
+                    <Input
+                        iconName='user'
+                        placeholder='Nome'
+                    />
+
+                    <Input
+                        iconName='mail'
+                        placeholder='E-mail'
+                        keyboardType='email-address'
+                    />
+
+                    <Input
+                        iconName='credit-card'
+                        placeholder='CNH'
+                        keyboardType='numeric'
+                    />
+
+                </Form>
+
+                <Button
+                    title='Próximo'
+                />
+
+            </Container>
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
     )
 }
--- src/screens/CriarConta/CriarContaPrimeiroPasso/styles.ts
+++ src/screens/CriarConta/CriarContaPrimeiroPasso/styles.ts
@@ -2,7 +2,6 @@ import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    flex:1;
     padding: 0 24px;
 
     background-color: ${({theme})=> theme.colors.background_primary};
@@ -37,6 +36,12 @@ export const SubTitle = styled.Text`
     color: ${({ theme }) => theme.colors.text};
 
     line-height: ${RFValue(25)}px;
+`
 
-    
+export const Form = styled.View``
+
+export const FormTitle = styled.Text`
+    font-size: ${RFValue(20)}px;
+    font-family: ${({theme})=>theme.fonts.secondary_600};
+    color: ${({ theme }) => theme.colors.text};
 `
