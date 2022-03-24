c2a0aa811c07fa23c63ea47867f2cff53761c24b
riansco14 <rian.rabelo@ufrpe.br>
2022-03-24 01:17:55 -0300

    Validação no segundo passo

--- src/screens/CriarConta/CriarContaSegundoPasso/index.tsx
+++ src/screens/CriarConta/CriarContaSegundoPasso/index.tsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import React, { useState } from 'react'
+import { Alert, Keyboard, KeyboardAvoidingView } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import { useTheme } from 'styled-components/native'
+import { BackButton } from '../../../components/BackButton'
+import { Bullet } from '../../../components/Bullet'
+import { Button } from '../../../components/Button'
+import { Input } from '../../../components/Input'
+import { InputPassword } from '../../../components/InputPassword'
 
-import {Container} from './styles'
+import { Container, Header, Steps, Title, SubTitle, Form, FormTitle } from './styles'
+
+interface Params {
+    user: {
+        name: string
+        email: string
+        numeroCNH: string
+    }
+}
 
 export function CriarContaSegundoPasso() {
-    return (
-        <Container>
+    const theme = useTheme()
+    const navigation = useNavigation()
+    const route = useRoute()
 
-        </Container>
+    const { user } = route.params as Params
+
+    const [password, setPassword] = useState('')
+    const [passwordConfirm, setPasswordConfirm] = useState('')
+
+    function handleBack() {
+        navigation.goBack()
+    }
+
+    function handleRegister() {
+        if (!password || !passwordConfirm) {
+            return Alert.alert('Informe a senha e a senha de confirmação')    
+        }
+
+        if (password !== passwordConfirm) {
+            return Alert.alert('As senhas não são iguais')    
+        }
+
+        //ENviar para API
+
+    }
+
+    return (<KeyboardAvoidingView behavior='position' enabled>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false} >
+            <Container>
+                <Header>
+                    <BackButton onPress={handleBack} />
+                    <Steps>
+                        <Bullet active />
+                        <Bullet />
+                    </Steps>
+                </Header>
+
+                <Title>
+                    Crie sua{'\n'}conta
+                </Title>
+
+                <SubTitle>
+                    Faça seu cadastro de{'\n'}
+                    forma rápida e fácil
+                </SubTitle>
+
+                <Form>
+                    <FormTitle>2. Senha</FormTitle>
+
+                    <InputPassword
+                        iconName='lock'
+                        placeholder='Senha'
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+                    <InputPassword
+                        iconName='lock'
+                        placeholder='Repetir Senha'
+                        value={passwordConfirm}
+                        onChangeText={setPasswordConfirm}
+                    />
+
+
+                </Form>
+
+                <Button
+                    title='Cadastrar'
+                    color={theme.colors.sucess}
+                    onPress={handleRegister}
+                />
+
+            </Container>
+        </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
     )
 }
--- src/screens/CriarConta/CriarContaSegundoPasso/styles.ts
+++ src/screens/CriarConta/CriarContaSegundoPasso/styles.ts
@@ -1,5 +1,52 @@
+import { RFValue } from 'react-native-responsive-fontsize'
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
-    flex:1;
+    padding: 0 24px;
+
+    background-color: ${({theme})=> theme.colors.background_primary};
+`
+
+export const Header = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-top: 40px;
+`
+
+export const Steps = styled.View`
+    flex-direction: row;
+
+`
+
+export const Title = styled.Text`
+    font-size: ${RFValue(40)}px;
+    font-family: ${({theme})=>theme.fonts.secondary_600};
+    color: ${({ theme }) => theme.colors.title};
+    
+    margin-top: 60px;
+    margin-bottom: 16px;
+`
+
+export const SubTitle = styled.Text`
+    font-size: ${RFValue(15)}px;
+    font-family: ${({theme})=>theme.fonts.primary_400};
+    color: ${({ theme }) => theme.colors.text};
+
+    line-height: ${RFValue(25)}px;
+`
+
+export const Form = styled.View`
+    width: 100%;
+    margin-top: 64px;
+    margin-bottom: 16px;
+`
+
+export const FormTitle = styled.Text`
+    font-size: ${RFValue(20)}px;
+    font-family: ${({theme})=>theme.fonts.secondary_600};
+    color: ${({ theme }) => theme.colors.text};
+    margin-bottom: 24px;
 `
