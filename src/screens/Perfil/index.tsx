e09a0840facc43fe4797236fde60bec04e7ded2c
riansco14 <rian.rabelo@ufrpe.br>
2022-04-05 00:21:15 -0300

    Header de Opções

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BackButton } from '../../components/BackButton'
 
-import { AvatarContainer, AvatarImage, Container, Content, Header, HeaderTop, IconContainer, Title } from './styles'
+import { AvatarContainer, AvatarImage, Container, Content, Header, HeaderTop, IconContainer, Title, OptionsContainer, Options, Option, OptionTitle } from './styles'
 import { Feather } from '@expo/vector-icons'
 import { useTheme } from 'styled-components'
 export function Perfil() {
     const theme = useTheme()
+
+    const [option, setOption] = useState<'dados' | 'senha'>('dados')
     return (
         <Container>
             <Header>
@@ -23,7 +25,32 @@ export function Perfil() {
                         <Feather color={theme.colors.white} size={24} name="camera" />
                     </IconContainer>
                 </AvatarContainer>
+
+                <OptionsContainer>
+                    <Options>
+                        <Option
+                            active={option == 'dados'}
+                            onPress={()=>setOption('dados')}
+                        >
+                            <OptionTitle
+                                active={option == 'dados'}>
+                                Dados
+                            </OptionTitle>
+                        </Option>
+                        <Option
+                            active={option == 'senha'}
+                            onPress={()=>setOption('senha')}
+                        >
+                            <OptionTitle
+                                active={option == 'senha'}>Trocar senha
+                            </OptionTitle>
+                        </Option>
+
+                    </Options>
+                </OptionsContainer>
             </Content>
+
+
         </Container>
     )
 }
--- src/screens/Perfil/styles.ts
+++ src/screens/Perfil/styles.ts
@@ -1,6 +1,10 @@
-import { RectButton } from 'react-native-gesture-handler'
+import { RectButton, TouchableOpacity } from 'react-native-gesture-handler'
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
+
+interface OptionProps {
+    active: boolean
+}
 
 export const Container = styled.View`
     flex:1;
@@ -10,7 +14,7 @@ export const Header = styled.View`
     height: ${RFPercentage(30)}px;
     width: 100%;
 
-    background-color: ${({theme})=>theme.colors.shape_dark};
+    background-color: ${({ theme }) => theme.colors.header};
 `
 
 export const HeaderTop = styled.View`
@@ -38,10 +42,9 @@ export const Content = styled.View`
 `
 
 export const AvatarContainer = styled.View`
-
+    margin-top: -90px;
     height: 180px;
     width: 180px;
-    top:-90px;
 `
 export const AvatarImage = styled.Image`
     height: 180px;
@@ -62,5 +65,38 @@ export const IconContainer = styled(RectButton)`
     align-items: center;
     justify-content: center;
     background-color: ${({ theme }) => theme.colors.main};
+
+`
+export const OptionsContainer = styled.View`
+    flex: 1;
+    padding: 0 24px;
+    margin-top: 32px;
+`
+
+export const Options = styled.View`
+    width: 100%;
+    border-bottom-width: 1px;
+    border-bottom-color: ${({ theme }) => theme.colors.line};
+    
+    flex-direction: row;
+    justify-content: space-around;
+
+    margin-bottom: 24px;
+
+`
+
+export const Option = styled(TouchableOpacity)<OptionProps>`
+    padding-bottom: 14px;
+    ${({ active }) => active && css`
+        border-bottom-color: ${({ theme }) => theme.colors.main};
+        border-bottom-width: 3px;
+    `}
+
+`
+
+export const OptionTitle = styled.Text<OptionProps>`
+    font-family: ${({ theme, active }) => active ? theme.fonts.secondary_600 : theme.fonts.secondary_500};
+    font-size: ${RFValue(20)}px;
+    color: ${({ theme, active }) => active ? theme.colors.title : theme.colors.text_detail};
 
 `
